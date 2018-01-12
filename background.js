131709296c739144647832cfe868c26e6479f93b
Ny'alotha <Nyalothas@gmail.com>
2018-01-12 17:58:28 +0200

    updated style and save options

--- background.js
+++ background.js
@@ -8,26 +8,21 @@ var storageObj = {
   txtColor: "",
 };
 
-getStorage();
-function getStorage(){
+updateIcon();
+function updateIcon() {
   chrome.storage.sync.get({
     bgColor: "",
     txtColor: "",
   }, function (items) {
-   storageObj.bgColor = items.bgColor;
-   storageObj.txtColor = items.txtColor;
-   console.log(storageObj.bgColor + " - " + storageObj.txtColor);
-  });
-}
+    storageObj.bgColor = items.bgColor;
+    storageObj.txtColor = items.txtColor;
 
-updateIcon();
-function updateIcon() {
-  chrome.tabs.query({}, function(tabs) {
-    var tab = tabs;
-    chrome.browserAction.setBadgeText({ text: tab.length.toString()});
-    chrome.browserAction.setTitle({ title: 'Opened Tabs: ' + tab.length.toString() });
-    draw(storageObj.bgColor,storageObj.txtColor,tab.length);
-    //draw(tab.length.toString());
+    chrome.tabs.query({}, function (tabs) {
+      var count = tabs.length.toString();
+      chrome.browserAction.setBadgeText({ text: count });
+      chrome.browserAction.setTitle({ title: 'Opened Tabs: ' + count });
+      draw(storageObj.bgColor, storageObj.txtColor, count);
+    });
   });
 }
 
@@ -48,22 +43,5 @@ function draw(bg = "#262626", txt = "#FFFFFF", text) {
 
   chrome.browserAction.setIcon({
     imageData: context.getImageData(0, 0, 19, 19)
-  });
-}
-
-function restore_options() {
-  chrome.storage.sync.get({
-    bgColor: "#262626",
-    txtColor: "#FFFFFF",
-    isBadgeVisible: isBadgeVisible
-  }, function (items) {
-    alert(isBadgeVisible);
-    chrome.browserAction.getBadgeText({}, function (result) {
-      if(!isBadgeVisible)
-        draw(item.bgColor,item.txtColor,"");
-        else
-        draw(item.bgColor,item.txtColor,result);
-    });
-    
   });
 }
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,33 +1,45 @@
 chrome.browserAction.getBadgeText({}, function (result) {
   $(".totalOpen").html(result);
+  $(".notify-badge").html(result);
 });
 
 var storageObj = {
   bgColor: "",
   txtColor: "",
-  isBadgeVisible: false
+  isBadgeVisible: ""
 };
 
 getStorage();
-function getStorage(){
+function getStorage() {
   chrome.storage.sync.get({
     bgColor: "",
     txtColor: "",
-    isBadgeVisible: false
+    isBadgeVisible: ""
   }, function (items) {
-   storageObj.bgColor = items.bgColor;
-   storageObj.txtColor = items.txtColor;
-   storageObj.isBadgeVisible = items.isBadgeVisible;
-   console.log(storageObj.bgColor + " - " + storageObj.txtColor + " > " + storageObj.isBadgeVisible);
-   $('#background').val(storageObj.bgColor);
-   $('#pickcolortxt').val(storageObj.txtColor);
+    UpdateStorage(items.bgColor, items.txtColor);
+    storageObj.isBadgeVisible = items.isBadgeVisible;
+    console.log(storageObj.bgColor + " - " + storageObj.txtColor + " > " + storageObj.isBadgeVisible);
   });
+
+/*   chrome.browserAction.getBadgeBackgroundColor( {}, function (ColorArray){
+    console.log(ColorArray);
+  }); */
+}
+
+function UpdateStorage(bg = storageObj.bgColor, txt = storageObj.txtColor) {
+  storageObj.bgColor = bg;
+  storageObj.txtColor = txt;
+  $('#pickcolorbg').val(bg).css("color", bg);
+  $('#pickcolortxt').val(txt).css("color", txt);
+
+  $('.totalOpen').css("background", bg);
+  $('.totalOpen').css("color", txt)
 }
 
 populateColors();
 function populateColors() {
   var colorList = [
-    '#bf3f7f', '#bf3f3f', '#bf7f3f', '#bfbf3f', '#7fbf3f', '#3fbf3f', '#3fbf7f', '#3fbfbf', '#3f7fbf', '#3f3fbf', '#7f3fbf', '#bf3fbf', '#FFFFFF', '#000000'
+    '#BF3F7F', '#BF3F3F', '#BF7F3F', '#BFBF3F', '#7FBF3F', '#3FBF3F', '#3FBF7F', '#3FBFBF', '#3F7FBF', '#3F3FBF', '#7F3FBF', '#BF3FBF', '#FFFFFF', '#000000'
   ];
 
   var pickerBg = $('#background');
@@ -47,10 +59,9 @@ pickerBg.children('div').hover(function () {
   } else {
     pickerBg.attr('disabled', false);
     var codeHex = $(this).data('hex');
-    $('#pickcolorbg').val(codeHex);
-    
-    storageObj.bgColor = codeHex;
-    draw(codeHex,storageObj.txtColor,$('.totalOpen')[0].innerText);
+
+    UpdateStorage(codeHex, undefined);
+    draw(codeHex, storageObj.txtColor, $('.totalOpen')[0].innerText);
   }
 });
 var pickerText = $('#textColor');
@@ -63,9 +74,16 @@ pickerText.children('div').hover(function () {
     var codeHex = $(this).data('hex');
     $('#pickcolortxt').val(codeHex);
 
-    storageObj.txtColor = codeHex;
+    UpdateStorage(undefined, codeHex);
     draw(storageObj.bgColor, codeHex, $('.totalOpen')[0].innerText);
   }
+});
+
+$("#pickcolorbg").on("change paste keyup", function() {
+  UpdateStorage($(this).val(), undefined);
+});
+$("#pickcolortxt").on("change paste keyup", function() {
+  UpdateStorage(undefined, $(this).val());
 });
 
 // Saves options to chrome.storage
@@ -93,24 +111,19 @@ function save_options() {
 // stored in chrome.storage.
 restore_options();
 function restore_options() {
-  // Use default value color = 'red' and txtColor = true.
   chrome.storage.sync.get({
-    bgColor: "#262626",
-    txtColor: "#FFFFFF"
+    bgColor: "",
+    txtColor: ""
   }, function (items) {
-    if(!items.bgColor){
+    if (!items.bgColor) {
       items.bgColor = "#262626";
     }
-    if(!items.txtColor){
+    if (!items.txtColor) {
       items.txtColor = "#FFFFFF";
     }
-    //document.getElementById('color').value = items.bgColor;
-    //document.getElementById('like').checked = items.txtColor;
-    storageObj.bgColor = items.bgColor;
-    storageObj.txtColor = items.txtColor;
-    $('#pickcolorbg').val(items.txtColor);
-    $('#pickcolortxt').val(items.bgColor);
-    draw(items.bgColor,items.txtColor,$('.totalOpen')[0].innerText);
+
+    UpdateStorage(items.bgColor, items.txtColor);
+    draw(items.bgColor, items.txtColor, $('.totalOpen')[0].innerText);
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
@@ -120,6 +133,7 @@ document.getElementById('save').addEventListener('click', save_options);
 document.getElementById('reset').addEventListener('click', reset);
 function reset() {
   draw(undefined, undefined, $('.totalOpen')[0].innerText);
+  UpdateStorage("#262626", "#FFFFFF");
 }
 
 function draw(bg = "#262626", txt = "#FFFFFF", text) {
@@ -163,7 +177,7 @@ function hideBadge() {
   //save status here
   chrome.storage.sync.set({
     isBadgeVisible: storageObj.isBadgeVisible
-  }, function () { });
+  }, function (items) {console.log(items + " <<") });
 
 }
 
@@ -173,12 +187,12 @@ locks.forEach(element => {
   element.addEventListener('click', function () {
     this.locked = !this.locked;
     element.setAttribute('locked-state', this.locked);
-  
+
     var input = element.previousElementSibling
     if (locked) {
       input.setAttribute('disabled', this.locked);
     } else
       input.removeAttribute('disabled');
-  
+
   }.bind(this));
 });
